a940b6400b827b5457d073d3d990e27f43b7ee27
rammohan <rammohan.1255@gmail.com>
2026-04-21 17:53:12 +0200

    first

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,67 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, FormsModule],
+  template: `
+    <h1>German Learning App</h1>
+    <p>Score: {{ score }} / {{ total }}</p>
+
+    <div *ngIf="word">
+      <p><b>German:</b> {{ word.german }}</p>
+
+      <input [(ngModel)]="userAnswer" placeholder="Type English word" />
+      <button (click)="checkAnswer()">Check</button>
+
+      <p *ngIf="result">{{ result }}</p>
+
+<p *ngIf="result">
+  <b>Answer:</b> {{ word.english }}
+</p>
+
+      <button (click)="loadWord()">Next Word</button>
+    </div>
+  `
 })
 export class App {
-  protected readonly title = signal('german-ui');
+  score: number = 0;
+total: number = 0;
+
+  word: any;
+  userAnswer: string = '';
+  result: string = '';
+
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {
+    this.loadWord();
+  }
+
+  loadWord() {
+    this.http.get('http://localhost:8081/api/word/random')
+      .subscribe(data => {
+        console.log("API DATA:", data);
+        this.word = data;
+        this.userAnswer = '';
+        this.result = '';
+        this.cd.detectChanges();
+      });
+  }
+
+
+checkAnswer() {
+  if (!this.word) return;
+
+  this.total++;
+
+  if (this.userAnswer.toLowerCase() === this.word.english.toLowerCase()) {
+    this.result = "✅ Correct!";
+    this.score++;
+  } else {
+    this.result = "❌ Wrong!";
+  }
+}
+  
 }
